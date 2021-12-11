11027b9d584b885980d57ba94decb5c833f88b3a
goncalveshigino <goncalveshigino20@gmail.com>
2021-12-12 00:33:07 +0100

    contolar o zoom

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,62 @@
+import { ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
+   styles: [
+    `
+    .mapa-container{
+
+       width: 100%;
+       height:100%;
+    
+    }
+
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+    `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  
+  mapa!: mapboxgl.Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    
+
+    this.mapa = new mapboxgl.Map({
+    container: this.divMapa.nativeElement, // container ID
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [13.501312833236417,-14.913656041004835],
+    zoom: 17
+   
+    });
+  
   }
+  
+
+  zoomOut() {
+    this.mapa.zoomOut();
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
+  }
+ 
+
 
 }
