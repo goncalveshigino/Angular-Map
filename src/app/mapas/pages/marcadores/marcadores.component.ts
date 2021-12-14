6ba5295f55e4ebe7b013856b3968a1c4f7033fa4
goncalveshigino <goncalveshigino20@gmail.com>
2021-12-14 21:34:04 +0100

    feito

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -2,6 +2,11 @@ import { AfterViewInit, ElementRef,ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+interface MarcadorColor {
+  color: string;
+  marker: mapboxgl.Marker;
+}
+
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
@@ -14,6 +19,17 @@ import * as mapboxgl from 'mapbox-gl';
     
     }
 
+    .list-group{
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index:99
+    }
+
+    li{
+      cursor: pointer;
+    }
+
     `
   ]
 })
@@ -23,6 +39,9 @@ export class MarcadoresComponent implements AfterViewInit {
   mapa!: mapboxgl.Map;
   zoomLevel: number = 15;
   center: [number, number] = [13.501312833236417, -14.913656041004835];
+
+  //Array de marcadores
+  marcadores: MarcadorColor[] = [];
 
   constructor() { }
 
@@ -36,16 +55,32 @@ export class MarcadoresComponent implements AfterViewInit {
     zoom: this.zoomLevel
    });
     
-    // const markerHtml: HTMLElement = document.createElement('div');
-    // markerHtml.innerHTML = "Hello world"
-    
-  //   new mapboxgl.Marker({
-  //    element: markerHtml
-  //  })
+  }
+
+  irMarcador() {
+
+    this.mapa.flyTo({
       
-       new mapboxgl.Marker()
+    })
+
+  }
+
+  addMarcador() {
+
+    const color = "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    
+    const novoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color: color
+    })
       .setLngLat(this.center)
       .addTo(this.mapa);
+    
+    //Add novo marcador no array
+    this.marcadores.push({
+      color,
+      marker: novoMarcador
+    });
 
   }
 
